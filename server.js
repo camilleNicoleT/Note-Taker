4f8617ef72cf36e7ed838f556869559e801b5b04
camilleNicole <camilleht@gmail.com>
2022-03-07 22:36:44 -0500

    connected Html to server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, notesArray) {
    let filteredResults = notesArray;
@@ -29,28 +30,24 @@ function findById(id, notesArray) {
   
 function createNewNote(body, notesArray) {
     const note = body;
+    console.log(body);
   notesArray.push(note);
     fs.writeFileSync(
       path.join(__dirname, './db/db.json'),
       JSON.stringify({notes:notesArray }, null, 2)
     );
-    returnNote;
+       return note;
   }
 
 function validateNote(note) {
-    if (!note.title || typeofNote.title !== 'string') {
+    if (!note.title || typeof note.title !== 'string') {
       return false;
+    } 
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
     }
-};
-
-app.get('/api/notes/:id', (req, res) => {
-const result = findById(req.params.id, notes);
-if (result) {
-    res.json(result);
- } else {
-    res.send(404);
+    return true;
 }
-});
 
 // //GET /notes should return the notes.html file.
 // app.get(/notes)
@@ -62,6 +59,15 @@ app.get('/api/notes', (req, res) => {
     res.json(results);
     });
 
+    app.get('/api/notes/:id', (req, res) => {
+        const result = findById(req.params.id, notes);
+        if (result) {
+            res.json(result);
+         } else {
+            res.send(404);
+        }
+        });
+        
 // // GET * should return the index.html file.
 // app.get(*)
 
@@ -70,7 +76,7 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
     // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();
-
+  
   // if any data in req.body is incorrect, send 400 error back
   if (!validateNote(req.body)) {
     res.status(400).send('The note is not properly formatted.');
@@ -81,6 +87,15 @@ const note = createNewNote(req.body, notes);
   }
   });
 
-  app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);});
